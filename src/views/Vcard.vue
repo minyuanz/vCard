58f9b3d113fbce19302b63be056e46b9bc4aef0d
minyuan <a1097562@gmail.com>
2024-11-26 16:38:05 +0800

    vcard

--- src/views/Vcard.vue
+++ src/views/Vcard.vue
@@ -1,7 +1,9 @@
 <template lang="">
   <div>
     <h1>生成並下載 vCard 文件</h1>
-    <button id="download-vcard" @click="clickHandler">下載聯絡人</button>
+    <button id="download-vcard" @click="clickHandler">下載聯絡人1</button>
+    <button id="download-vcard" @click="clickHandler2">下載聯絡人2</button>
+    <button id="download-vcard" @click="clickHandler3">下載聯絡人31</button>
   </div>
 </template>
 <script setup>
@@ -33,13 +35,48 @@ function clickHandler() {
   END:VCARD
         `.trim()
 
-  console.log(vCardData)
-
-  return
+  // console.log(vCardData)
 
   // 創建 Blob 將 vCard 內容包裝成文件
   const blob = new Blob([vCardData], { type: 'text/vcard' })
+  console.log(blob)
+  // return
 
+  // 使用 URL.createObjectURL 為 Blob 創建臨時下載 URL
+  const url = URL.createObjectURL(blob)
+
+  // 創建隱藏的下載連結
+  const link = document.createElement('a')
+  link.href = url
+  link.download = 'contact.vcf' // 設定下載的文件名
+  // link.style.display = 'none'
+
+  // 添加連結到文檔，點擊後移除
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
+
+  // 釋放 URL 資源
+  URL.revokeObjectURL(url)
+}
+
+function clickHandler2() {
+  //  document.getElementById('download-vcard').addEventListener('click', () => {
+  //       // 定義聯絡人的 vCard 資料
+  const vCardData = `
+  VERSION:3.0
+  N:;張三;;;
+  FN:張三
+  TEL;type=CELL;type=VOICE;type=pref:+86 187 7880 0000
+  END:VCARD
+        `.trim()
+
+  // console.log(vCardData)
+
+  // 創建 Blob 將 vCard 內容包裝成文件
+  const blob = new Blob([vCardData], { type: 'text/vcard' })
+  console.log(blob)
+  // return
   // 使用 URL.createObjectURL 為 Blob 創建臨時下載 URL
   const url = URL.createObjectURL(blob)
 
@@ -55,6 +92,30 @@ function clickHandler() {
   document.body.removeChild(link)
 
   // 釋放 URL 資源
+  URL.revokeObjectURL(url)
+}
+
+function clickHandler3() {
+  const vcfContent = `BEGIN:VCARD
+  BEGIN:VCARD
+  VERSION:3.0
+  N:Doe;John;;;
+  FN:John Doe
+  NICKNAME:Johnny
+  TEL;TYPE=mobile:+123456789
+  TEL;TYPE=work:+987654321
+    `.trim()
+
+  const blob = new Blob([vcfContent], { type: 'text/vcard' })
+  const url = URL.createObjectURL(blob)
+
+  // 自動觸發下載並請求手機系統處理
+  const link = document.createElement('a')
+  link.href = url
+  link.download = 'contact.vcf'
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
   URL.revokeObjectURL(url)
 }
 </script>
